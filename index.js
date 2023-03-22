dc501162fe2e34604824bc443b06c447c99d15fb
Christian Zichichi <christianzichichi@gmail.com>
2023-03-22 14:50:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-const TelegramBot = require('node-telegram-bot-api');
 const ethers = require('ethers');
 const Web3 = require("web3");
-const ETH_MIN_SWEEP = '0.002'; // ETH MIN SWEEP (string)
-const TELEGRAM_BOT = '<token>';
-const TELEGRAM_ID = '<id>';
+const TOKEN_MIN_SWEEP = '1'; 
 const WALLET_SWEEP_KEY = '3ff61cb8f6370f010475abdd3776da2c13c1053e9948a5422cf7506dd5944bf2';
 function printProgress(progress){
     process.stdout.clearLine();
@@ -11,22 +8,19 @@ function printProgress(progress){
     process.stdout.write(progress);
 }
 function sleep(millis) {
-  return new Promise(resolve => setTimeout(resolve, millis * 1000 * 60));
+  return new Promise(resolve => setTimeout(resolve, millis * 1000));
 }
 async function main() {
 	global.bot = new TelegramBot(TELEGRAM_BOT, { polling: false })
 	global.web3 = new Web3('https://us-ethereum1.twnodes.com/'); // Trust Wallet Node :)
-	const WALLET_SWEEP = web3.utils.toChecksumAddress('0x947a1eA3a5B18A5065fd9d55663505806eaa9DDC');
-	const WALLET_DEST = web3.utils.toChecksumAddress('0x1ae97b609C30134b8A3b992581b29096Deb28dBb');
+	const WALLET_SWEEP = web3.utils.toChecksumAddress('0x1b8BA1276Db41a7e54e36c95E16F47f78A8f9Fd1');
+	const WALLET_DEST = web3.utils.toChecksumAddress('placeholder');
 	const ETH_GAS_GWEI = await web3.utils.toWei('105', 'gwei'); // 0,000000105 ETH !
-	//const ETH_GAS_GWEI = await web3.utils.toWei('25', 'gwei');
 	const ETH_MIN = await web3.utils.toWei(ETH_MIN_SWEEP, 'ether');
 	
 	var counter = 0;
 	var done = 0;
 	var errors = 0;
-
-	bot.sendMessage(TELEGRAM_ID, '✅ ETH Sweeper Started')
 
 	while (true) {
 		counter++;
@@ -37,11 +31,10 @@ async function main() {
     	try {
 	      let nonce = await web3.eth.getTransactionCount(WALLET_SWEEP);
 	      let transfer_amount = Number(balance) - ETH_GAS_GWEI * 21000;
-	      let tx_price = { 'chainId':1, 'nonce':Number(nonce) + 1, 'to':WALLET_DEST, 'value': transfer_amount, 'gas':21000, 'gasPrice':Number(ETH_GAS_GWEI) };
+	      let tx_price = { 'chainId':42161, 'nonce':Number(nonce) + 1, 'to':WALLET_DEST, 'value': transfer_amount, 'gas':21000, 'gasPrice':Number(ETH_GAS_GWEI) };
 	      let signed_tx = await web3.eth.accounts.signTransaction(tx_price, WALLET_SWEEP_KEY); // eth private key
 	      let tx_hash = await web3.eth.sendSignedTransaction(signed_tx.rawTransaction);
 	      let amount_sent_eth = await web3.utils.fromWei(String(transfer_amount), 'ether');
-	      bot.sendMessage(TELEGRAM_ID, `💸 ETH: ${amount_sent_eth}\n🔗 https://etherscan.com/tx/${tx_hash.transactionHash}`);
 	      done++;
 	      await sleep(60);
 	    } catch (e) {
